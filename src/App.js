399cd636d51f1c8d5c5e068b8be14fb3bbe67520
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-11-29 18:28:00 -0300

    Diseño de GUI sencilla para permitir al user interactuar con los modelos de Computer Vision de Azure IA

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+export const App = () => {
+  const [imageUrl, setImageUrl] = useState("");
 
-export default App;
+  const handleImageAnalysis = () => {
+    // Lógica para analizar la imagen
+    console.log("Análisis de imagen");
+  };
+
+  const handleImageGeneration = () => {
+    // Lógica para generar la imagen
+    console.log("Generación de imagen");
+  };
+
+  return (
+    <div>
+      <h1>Aplicación de Computer Vision</h1>
+
+      <label>
+        Dirección URL de la imagen:
+        <input
+          type="text"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+      </label>
+
+      <br />
+
+      <button onClick={handleImageAnalysis}>Analizar Imagen</button>
+      <button onClick={handleImageGeneration}>Generar Imagen</button>
+    </div>
+  );
+};
